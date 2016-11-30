78b62e03d5a627fe42fe28f8fb53c4a7e3a1edbc
vimalaad <vimalaad@gmail.com>
2016-12-01 00:20:35 +0330

    make master one commit ahead of nightly

--- deps/build.jl
+++ deps/build.jl
@@ -29,8 +29,8 @@ catch err
 end
 try
   println("CoolProp latestVersion = $latestVersion_CoolProp ...")
-  coolpropurlbase = "http://www.coolprop.dreamhosters.com/binaries/"
-  _download(coolpropurlbase * "Julia/CoolProp.jl", joinpath(destpathbase,"CoolProp.jl"));
+  coolpropurlbase = "http://netix.dl.sourceforge.net/project/coolprop/CoolProp/$latestVersion_CoolProp/";
+  _download(coolpropurlbase * "Julia/CoolProp.jl", joinpath(destpathbase, "CoolProp.jl"));
   info("I'm Getting CoolProp Binaries...");
   @static if is_windows()
     urlbase = coolpropurlbase * "shared_library/Windows/$OS_ARCH_CoolProp/";
@@ -39,7 +39,6 @@ try
     _download(joinpath(urlbase,"exports.txt"), joinpath(destpathbase,"exports.txt"));
   end
   @static if is_linux()
-    coolpropurlbase = "http://netix.dl.sourceforge.net/project/coolprop/CoolProp/nightly/";
     urlbase = coolpropurlbase * "shared_library/Linux/$OS_ARCH_CoolProp/libCoolProp.so.$latestVersion_CoolProp";
     _download(urlbase, joinpath(destpathbase,"CoolProp.so"));
   end

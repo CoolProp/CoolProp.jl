e83c1560bc2ab481b3541e425a840a00fec1980e
Reza Afzalan <rafzalan@gmail.com>
2017-01-10 22:04:05 +0330

    eval export & const

--- test/CoolProp.jl
+++ test/CoolProp.jl
@@ -1,13 +1,7 @@
 module CoolProp
 using Compat
-export PropsSI, PhaseSI, get_global_param_string, get_parameter_information_string, get_fluid_param_string, set_reference_stateS, get_param_index, get_input_pair_index, set_config, F2K, K2F, HAPropsSI, AbstractState_factory, AbstractState_free, AbstractState_set_fractions, AbstractState_update, AbstractState_specify_phase, AbstractState_unspecify_phase, AbstractState_keyed_output, AbstractState_output, AbstractState_update_and_common_out, AbstractState_update_and_1_out, AbstractState_update_and_5_out, AbstractState_set_binary_interaction_double, AbstractState_set_cubic_alpha_C, AbstractState_set_fluid_parameter_double
-export propssi, phasesi, k2f, f2k, hapropssi, cair_sat, set_reference_state
-export abstractstate_factory, abstractstate_free, abstractstate_set_fractions, abstractstate_update, abstractstate_keyed_output, abstractstate_specify_phase, abstractstate_unspecify_phase, abstractstate_update_and_common_out!, abstractstate_update_and_5_out!, abstractstate_update_and_1_out!, abstractstate_set_binary_interaction_double, abstractstate_set_cubic_alpha_c, abstractstate_set_fluid_parameter_double, abstractstate_output
-export saturation_ancillary, set_departure_functions
-export AbstractState_first_saturation_deriv, AbstractState_first_partial_deriv, AbstractState_build_phase_envelope, AbstractState_build_spinodal, AbstractState_get_phase_envelope_data
-export abstractstate_first_saturation_deriv, abstractstate_first_partial_deriv, abstractstate_build_phase_envelope, abstractstate_get_phase_envelope_data!, abstractstate_build_spinodal, abstractstate_get_spinodal_data!, abstractState_all_critical_points!
-errcode = Ref{Clong}(0)
 
+errcode = Ref{Clong}(0)
 const buffer_length = 20000
 message_buffer = Array(UInt8, buffer_length)
 
@@ -48,30 +42,16 @@ end
 include("CoolPropHA.jl")
 include("CoolPropLowLevel.jl")
 
-const PropsSI = propssi
-const PhaseSI = phasesi
-const K2F = k2f
-const F2K = f2k
-const HAPropsSI = hapropssi
-const AbstractState_factory = abstractstate_factory
-const AbstractState_free = abstractstate_free
-const AbstractState_set_fractions = abstractstate_set_fractions
-const AbstractState_update = abstractstate_update
-const AbstractState_keyed_output = abstractstate_keyed_output
-const AbstractState_output = abstractstate_output
-const AbstractState_specify_phase = abstractstate_specify_phase
-const AbstractState_unspecify_phase = abstractstate_unspecify_phase
-const AbstractState_update_and_common_out = abstractstate_update_and_common_out!
-const AbstractState_update_and_1_out = abstractstate_update_and_1_out!
-const AbstractState_update_and_5_out = abstractstate_update_and_5_out!
-const AbstractState_set_binary_interaction_double = abstractstate_set_binary_interaction_double
-const AbstractState_set_cubic_alpha_C = abstractstate_set_cubic_alpha_c
-const AbstractState_set_fluid_parameter_double = abstractstate_set_fluid_parameter_double
+for sym=[:PropsSI, :PhaseSI, :K2F, :F2K, :HAPropsSI, :AbstractState_factory, :AbstractState_free, :AbstractState_set_fractions, :AbstractState_update, :AbstractState_keyed_output, :AbstractState_output, :AbstractState_specify_phase, :AbstractState_unspecify_phase, :AbstractState_update_and_common_out, :AbstractState_update_and_1_out, :AbstractState_update_and_5_out, :AbstractState_set_binary_interaction_double, :AbstractState_set_cubic_alpha_C, :AbstractState_set_fluid_parameter_double, :AbstractState_first_saturation_deriv, :AbstractState_first_partial_deriv, :AbstractState_build_phase_envelope, :AbstractState_build_spinodal]
+  symorigin = Symbol(replace(lowercase(string(sym)),r"out$","out!"))
+  @eval const $sym = $symorigin
+  @eval export $sym, $symorigin
+end
 const set_reference_stateS = set_reference_state
 const set_reference_stateD = set_reference_state
-const AbstractState_first_saturation_deriv = abstractstate_first_saturation_deriv
-const AbstractState_first_partial_deriv = abstractstate_first_partial_deriv
-const AbstractState_build_phase_envelope = abstractstate_build_phase_envelope
-const AbstractState_build_spinodal = abstractstate_build_spinodal
 const AbstractState_get_phase_envelope_data = abstractstate_get_phase_envelope_data!
+export set_reference_stateS, set_reference_stateD, AbstractState_get_spinodal_data, AbstractState_all_critical_points, AbstractState_get_phase_envelope_data
+export set_reference_state, abstractstate_get_spinodal_data!, abstractState_all_critical_points!, abstractstate_get_phase_envelope_data!
+export get_global_param_string, get_parameter_information_string, get_fluid_param_string, get_param_index, get_input_pair_index, set_config
+export saturation_ancillary, set_departure_functions
 end #module

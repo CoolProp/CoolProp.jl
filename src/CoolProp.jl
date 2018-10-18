2146e373432218abc7de88e28b9cba285e407c05
Alberto Lazzarotto <alberto.lazzarotto84@gmail.com>
2018-10-18 17:13:40 +0200

    modify the enry file CoolPprop.jl

--- src/CoolProp.jl
+++ src/CoolProp.jl
@@ -1,13 +1,27 @@
+import Libdl
 push!(Libdl.DL_LOAD_PATH, abspath(@__FILE__, "..", "..", "deps", "lib"));
 
-if (haskey(ENV, "includelocalwrapper") && ENV["includelocalwrapper"]=="on")
-  haskey(ENV, "TestingPath") || (ENV["TestingPath"] = abspath(@__FILE__, "..", "..", "test/CoolProp.jl"));
-  if (isfile(ENV["TestingPath"]))
-    include(ENV["TestingPath"]);
-    warn("** TestingMode is enabeled for: " * ENV["TestingPath"] * " **");
-  else
-    warn("** $(ENV["TestingPath"]) not exists **");
-  end
-else
-  include("../deps/lib/CoolProp.jl");
+module CoolProp
+
 end
+
+# push!(LOAD_PATH, abspath(@__FILE__, "..", "..", "deps", "lib"));
+
+# if (haskey(ENV, "includelocalwrapper") && ENV["includelocalwrapper"]=="on")
+#   haskey(ENV, "TestingPath") || (ENV["TestingPath"] = abspath(@__FILE__, "..", "..", "test/CoolProp.jl"));
+#   if (isfile(ENV["TestingPath"]))
+#     include(ENV["TestingPath"]);
+#     warn("** TestingMode is enabeled for: " * ENV["TestingPath"] * " **");
+#   else
+#     warn("** $(ENV["TestingPath"]) not exists **");
+#   end
+# else
+#   println()
+#   println(abspath(@__FILE__, "..", "..", "deps/lib/CoolProp.jl"))
+#   println()
+#   # Base.include(Main,abspath(@__FILE__, "..", "..", "deps/lib/CoolProp.jl"))    
+
+# end
+
+
+
